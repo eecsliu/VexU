f931b0d347727943d019febed248257bce1a1fab
Eric Liu <rickchang@berkeley.edu>
2019-02-02 17:43:46 -0800

    Sam did most of this

--- robot/kinect/kinect.py
+++ robot/kinect/kinect.py
@@ -4,22 +4,39 @@ import frame_convert2
 import numpy as np
 import argparse
 import imutils
+import shapedetector
 
-cv2.namedWindow('Depth')
-cv2.namedWindow('RGB')
+cv2.namedWindow('Blue')
 cv2.namedWindow('Red')
 keep_running = True
 
 
-def display_depth(dev, data, timestamp):
+'''def display_depth(dev, data, timestamp):
     global keep_running
-    imgray = cv2.cvtColor(frame_convert2.pretty_depth_cv(data),cv2.COLOR_BGR2GRAY)
-    ret,thresh = cv2.threshold(imgray,127,255,0)
-    contours, heirarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-    cv2.drawContours(frame_convert2.pretty_depth_cv(data),contours,-1,(0,255,0),3)
-    cv2.imshow('Depth', frame_convert2.pretty_depth_cv(data))
+    #imgray = cv2.cvtColor(frame_convert2.pretty_depth_cv(data),cv2.COLOR_BGR2GRAY)
+    #ret,thresh = cv2.threshold(imgray,127,255,0)
+    #im2, contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+    #cv2.drawContours(frame_convert2.pretty_depth_cv(data), contours, -1, (0,255,0), 3)
+    
+    image = frame_convert2.pretty_depth_cv(data)
+    resized = imutils.resize(image, width=300)
+    ratio = image.shape[0] / float(resized.shape[0])
+ 
+    # convert the resized image to grayscale, blur it slightly,
+    # and threshold it
+    gray = cv2.cvtColor(resized, cv2.COLOR_BGR2GRAY)
+    blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+    thresh = cv2.threshold(blurred, 60, 255, cv2.THRESH_BINARY)[1]
+ 
+    # find contours in the thresholded image and initialize the
+    # shape detector
+    cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
+	cv2.CHAIN_APPROX_SIMPLE)
+    cnts = imutils.grab_contours(cnts)
+    sd = ShapeDetector()
+    #cv2.imshow('Depth', frame_convert2.pretty_depth_cv(data))
     if cv2.waitKey(10) == 27:
-        keep_running = False
+        keep_running = False'''
 
 
 def display_rgb(dev, data, timestamp):
@@ -45,13 +62,15 @@ def findCountours():
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--image", help = "path to the image")
 args = vars(ap.parse_args())
-
-boundaries = [[0, 0, 0], [90, 255, 255]]
+#[greenVal, blueVal, redVal]
+boundaries = [[0, 0, 30], [60, 30, 255], #red
+	      [10, 10, 0], [180, 255, 80]] #blue
 
 def display_red(dev, data, timestamp):
 	global keep_running
 	lower = np.array(boundaries[0], dtype = "uint8")
 	upper = np.array(boundaries[1], dtype = "uint8")
+
  
 	# find the colors within the specified boundaries and apply
 	# the mask
@@ -61,8 +80,23 @@ def display_red(dev, data, timestamp):
 	if cv2.waitKey(10) == 27:
 		keep_running = False
 
+def display_blue(dev, data, timestamp):
+	global keep_running
+	lower = np.array(boundaries[2], dtype = "uint8")
+	upper = np.array(boundaries[3], dtype = "uint8")
+ 
+	# find the colors within the specified boundaries and apply
+	# the mask
+	mask = cv2.inRange(frame_convert2.video_cv(data), lower, upper)
+	output = cv2.bitwise_and(frame_convert2.video_cv(data), frame_convert2.video_cv(data), mask = mask)
+	cv2.imshow('Blue', output)
+	if cv2.waitKey(10) == 27:
+		keep_running = False
+
 print('Press ESC in window to stop')
-freenect.runloop(depth=display_depth,
-                 #video=display_rgb,
+freenect.runloop(#video=display_rgb,
                  video=display_red,
+                 body=body)
+freenect.runloop(#video=display_rgb,
+                 video=display_blue,
                  body=body)
