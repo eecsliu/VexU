13d411b430484d543128731410d3848100ef8500
sofatso <samuelchen173@gmail.com>
2019-02-25 20:45:39 -0800

    Added back all the kinect code

--- robot/kinect/test.py
+++ robot/kinect/test.py
@@ -4,15 +4,31 @@ import frame_convert2
 import numpy as np
 import shapedetector
 from scipy.spatial import distance as dist
-from imutils import perspective
-from imutils import contours
-import imutils
+import RPi.GPIO as GPIO            # import RPi.GPIO module  
+from time import sleep
+#from imutils import perspective
+#from imutils import contours
+#import imutils
+
+GPIO.setmode(GPIO.BCM)
+FORWARD = 8
+BACKWARD = 18
+LEFT = 22
+RIGHT = 23
+GPIO.setup(FORWARD, GPIO.OUT)
+GPIO.setup(BACKWARD, GPIO.OUT)
+GPIO.setup(LEFT, GPIO.OUT)
+GPIO.setup(RIGHT, GPIO.OUT)
 
 keep_running = True
 red = True
 iter = 0
+avg = []
+size = 5
 
 def displayRedOrBlue(dev, data, timestamp):
+        GPIO.output(FORWARD, 1)
+            
         global keep_running
         global red
         global iter
@@ -66,27 +82,29 @@ def displayRedOrBlue(dev, data, timestamp):
                 num = x
 	
         rect = cv2.minAreaRect(cnts[num])
-        box = cv2.boxPoints(rect)
+        box = cv2.cv.BoxPoints(rect)
         box = np.int0(box)
-        print(box)
-        print(midpoint(box[0],box[2]))
-        cv2.drawContours(output,[box],0,(0,0,255),2) 
-        output = cv2.cvtColor(output, cv2.COLOR_BGR2GRAY)
-        circles = cv2.HoughCircles(output, cv2.HOUGH_GRADIENT, 1, 50, param1=50,param2=35,minRadius=20,maxRadius=50)
-        if circles is not None:
+        avg.append(midpoint(box[0],box[2]))
+        print(np.sum(avg)/size)
+        if len(avg) >= size:
+                avg.pop(0)
+        #cv2.drawContours(output,[box],0,(0,0,255),2) 
+        #output = cv2.cvtColor(output, cv2.COLOR_BGR2GRAY)
+        #circles = cv2.HoughCircles(output, cv2.HOUGH_GRADIENT, 1, 50, param1=50,param2=35,minRadius=20,maxRadius=50)
+        #if circles is not None:
 	# convert the (x, y) coordinates and radius of the circles to integers
-            circles = np.round(circles[0, :]).astype("int")
+        #    circles = np.round(circles[0, :]).astype("int")
  
 	# loop over the (x, y) coordinates and radius of the circles
-            for (x, y, r) in circles:
+         #   for (x, y, r) in circles:
 		# draw the circle in the output image, then draw a rectangle
 		# corresponding to the center of the circle
-                cv2.circle(output, (x, y), r, (0, 255, 0), 4)
-                cv2.rectangle(output, (x - 5, y - 5), (x + 5, y + 5), (0, 128, 255), -1)
+         #       cv2.circle(output, (x, y), r, (0, 255, 0), 4)
+         #       cv2.rectangle(output, (x - 5, y - 5), (x + 5, y + 5), (0, 128, 255), -1)
  
-        cv2.imshow("Line Boi", output)
-        if cv2.waitKey(10) == 27:
-            keep_running = False
+        #cv2.imshow("Line Boi", output)
+        #if cv2.waitKey(10) == 27:
+         #   keep_running = False
 
 def body(*args):
     if not keep_running:
